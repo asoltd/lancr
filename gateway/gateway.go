3319b0a7a6401097f2d8b1fb75fdccc60ee65c59
Piotr Ostrowski <piotrostr@google.com>
2023-09-30 20:39:46 +0200

    refactor: move the gateway to separate module

---
+++ gateway/gateway.go
@@ -0,0 +1,68 @@
+package gateway
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+	"mime"
+	"net/http"
+	"strings"
+
+	"github.com/asoltd/lancr/gen"
+	lancrv1 "github.com/asoltd/lancr/gen/go/proto/lancr/v1"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+)
+
+// LICENSE: MIT
+// CREDIT: github.com/johanbrandhorst/grpc-gateway-boilerplate
+// getOpenAPIHandler serves an OpenAPI UI.
+// Adapted from https://github.com/philips/grpc-gateway-example/blob/a269bcb5931ca92be0ceae6130ac27ae89582ecc/cmd/serve.go#L63
+// there is the embed package used in reference implementation but it requires
+// putting a file in a generated-code directory
+func getOpenAPIHandler() (http.Handler, error) {
+	mime.AddExtensionType(".svg", "image/svg+xml")
+	// Use subdirectory in embedded files
+	subFS, err := fs.Sub(gen.OpenAPI, "openapiv2")
+	if err != nil {
+		return nil, err
+	}
+	return http.FileServer(http.FS(subFS)), nil
+}
+
+func Run(ctx context.Context, dialAddr string, gatewayAddr string) error {
+	conn, err := grpc.DialContext(
+		context.Background(),
+		dialAddr,
+		grpc.WithInsecure(),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		return err
+	}
+
+	gwmux := runtime.NewServeMux()
+	err = lancrv1.RegisterHeroServiceHandler(ctx, gwmux, conn)
+	if err != nil {
+		return err
+	}
+
+	oa, err := getOpenAPIHandler()
+	if err != nil {
+		return err
+	}
+
+	gwServer := &http.Server{
+		Addr: gatewayAddr,
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// could use /api here rather than /v1, I think is smarter to do long-term
+			if strings.HasPrefix(r.URL.Path, "/v1") {
+				gwmux.ServeHTTP(w, r)
+				return
+			}
+			oa.ServeHTTP(w, r)
+		}),
+	}
+
+	return fmt.Errorf("gRPC-Gateway server: %w", gwServer.ListenAndServe())
+}
--- main.go
+++ main.go
@@ -5,20 +5,15 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"io/fs"
 	"log"
-	"mime"
 	"net"
-	"net/http"
 	"os"
-	"strings"
 
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/reflection"
 
-	"github.com/asoltd/lancr/gen"
+	"github.com/asoltd/lancr/gateway"
 	lancrv1 "github.com/asoltd/lancr/gen/go/proto/lancr/v1"
 	"github.com/asoltd/lancr/server"
 )
@@ -38,60 +33,6 @@ import (
 // - [ ] Ensure idempotency of the application (no double withdrawals),
 // transactional support should be enabled by default in Firestore since it is
 // based in Spanner
-
-// LICENSE: MIT
-// CREDIT: github.com/johanbrandhorst/grpc-gateway-boilerplate
-// getOpenAPIHandler serves an OpenAPI UI.
-// Adapted from https://github.com/philips/grpc-gateway-example/blob/a269bcb5931ca92be0ceae6130ac27ae89582ecc/cmd/serve.go#L63
-// there is the embed package used in reference implementation but it requires
-// putting a file in a generated-code directory
-func getOpenAPIHandler() (http.Handler, error) {
-	mime.AddExtensionType(".svg", "image/svg+xml")
-	// Use subdirectory in embedded files
-	subFS, err := fs.Sub(gen.OpenAPI, "openapiv2")
-	if err != nil {
-		return nil, err
-	}
-	return http.FileServer(http.FS(subFS)), nil
-}
-
-func runGateway(ctx context.Context, dialAddr string, gatewayAddr string) error {
-	conn, err := grpc.DialContext(
-		context.Background(),
-		dialAddr,
-		grpc.WithInsecure(),
-		grpc.WithBlock(),
-	)
-	if err != nil {
-		return err
-	}
-
-	gwmux := runtime.NewServeMux()
-	err = lancrv1.RegisterHeroServiceHandler(ctx, gwmux, conn)
-	if err != nil {
-		return err
-	}
-
-	oa, err := getOpenAPIHandler()
-	if err != nil {
-		return err
-	}
-
-	gwServer := &http.Server{
-		Addr: gatewayAddr,
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// could use /api here rather than /v1, I think is smarter to do long-term
-			if strings.HasPrefix(r.URL.Path, "/v1") {
-				gwmux.ServeHTTP(w, r)
-				return
-			}
-			oa.ServeHTTP(w, r)
-		}),
-	}
-
-	return fmt.Errorf("gRPC-Gateway server: %w", gwServer.ListenAndServe())
-}
-
 func run() error {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
@@ -127,7 +68,7 @@ func run() error {
 
 	// Start HTTP server (and proxy calls to gRPC server endpoint)
 	log.Infof("Serving gRPC-Gateway and OpenAPI Documentation on https://%s", gatewayAddr)
-	return runGateway(ctx, grpcServerAddr, gatewayAddr)
+	return gateway.Run(ctx, grpcServerAddr, gatewayAddr)
 }
 
 func main() {
