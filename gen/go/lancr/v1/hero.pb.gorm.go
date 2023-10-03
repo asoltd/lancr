17c5d2e26e1154fac580b02334a4cd2d18167fc5
Piotr Ostrowski <piotrostr@google.com>
2023-10-03 18:12:18 +0200

    bring back params (page-token etc)

--- gen/go/lancr/v1/hero.pb.gorm.go
+++ gen/go/lancr/v1/hero.pb.gorm.go
@@ -3,12 +3,13 @@ package v1
 import (
 	context "context"
 	fmt "fmt"
+	strings "strings"
+
 	gateway "github.com/infobloxopen/atlas-app-toolkit/gateway"
 	gorm1 "github.com/infobloxopen/atlas-app-toolkit/gorm"
 	errors "github.com/infobloxopen/protoc-gen-gorm/errors"
-	gorm "github.com/jinzhu/gorm"
 	field_mask "google.golang.org/genproto/protobuf/field_mask"
-	strings "strings"
+	"gorm.io/gorm"
 )
 
 type HeroORM struct {
