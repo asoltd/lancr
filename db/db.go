8f0de33370c254e1e96b3c9b5c586343ed71740c
Piotr Ostrowski <piotrostr@google.com>
2023-10-05 19:05:38 +0200

    use hosted clickhouse DB

--- db/db.go
+++ db/db.go
@@ -1,8 +1,14 @@
 package db
 
 import (
+	"crypto/tls"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/ClickHouse/clickhouse-go/v2"
 	lancrv1 "github.com/asoltd/lancr/gen/go/lancr/v1"
-	"gorm.io/driver/clickhouse"
+	gormclickhouse "gorm.io/driver/clickhouse"
 	"gorm.io/gorm"
 )
 
@@ -14,9 +20,36 @@ func Connect() (*gorm.DB, error) {
 // add support for cloud requires running ./clickhouse server, otherwise this
 // function will fail
 func ConnectClickhouse() (*gorm.DB, error) {
-	dsn := "tcp://localhost:9000"
-	dialector := clickhouse.Open(dsn)
-	db, err := gorm.Open(dialector, &gorm.Config{})
+	// maybe do if unset variable then run locally or some passwordless version?
+	password, ok := os.LookupEnv("CLICKHOUSE_PASSWORD")
+	if !ok {
+		return nil, fmt.Errorf("CLICKHOUSE_PASSWORD is not set")
+	}
+	sqlDB := clickhouse.OpenDB(&clickhouse.Options{
+		Addr: []string{"z5o0xx7mhd.europe-west4.gcp.clickhouse.cloud:9440"},
+		Auth: clickhouse.Auth{
+			Database: "default",
+			Username: "default",
+			Password: password,
+		},
+		TLS: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+		Settings: clickhouse.Settings{
+			"max_execution_time": 60,
+		},
+		DialTimeout: 10 * time.Second,
+		Debug:       false,
+	})
+
+	err := sqlDB.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := gorm.Open(gormclickhouse.New(gormclickhouse.Config{
+		Conn: sqlDB,
+	}))
 	if err != nil {
 		return nil, err
 	}
